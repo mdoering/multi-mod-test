0d73146535c4412d5af659dcae222f9d290e0f2b
Markus Döring <m.doering@mac.com>
2015-11-19 15:21:31 +0100

    Create inter dependent modules

--- mod1/src/main/java/org/gbif/test/Test1.java
+++ mod1/src/main/java/org/gbif/test/Test1.java
@@ -1,8 +1,11 @@
 package org.gbif.test;
 
+import org.gbif.test.mod3.Test3;
+
 /**
  * test 1 class
  */
 public class Test1 {
-    public static String NAME = "TEST1";
+  public static String NAME = "TEST1";
+  private static String N1 = Test3.NAME;
 }
--- mod2/src/main/java/org/gbif/test/Test2.java
+++ mod2/src/main/java/org/gbif/test/Test2.java
@@ -4,5 +4,7 @@ package org.gbif.test;
  * test 2 class
  */
 public class Test2 {
-    public static String NAME = "TEST2";
+  public static String NAME = "TEST2";
+  private static String N1 = Test1.NAME;
+
 }
